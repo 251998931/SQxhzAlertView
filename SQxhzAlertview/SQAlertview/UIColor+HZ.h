cdafe359a8aa18ff6c8f5cc8d9ce52043be67706
小孩子 <251998931@qq.com>
2016-04-13 16:35:20 +0800

    first commit2016

---
+++ SQxhzAlertview/SQAlertview/UIColor+HZ.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+HZ.h
+//  xiaohaizi
+//
+//  Created by 小孩子 on 16/3/24.
+//  Copyright © 2016年 sks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HZ)
++ (UIColor *)colorWithHexString:(NSString *)color;
+@end
---
+++ SQxhzAlertview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQxhzAlertview
+//
+//  Created by 小孩子 on 16/4/13.
+//  Copyright © 2016年 com.shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
